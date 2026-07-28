eb53a140d1fde2d181532cab3f3828f8a713436d
Daniil Panov <daniil_panov2005@mail.ru>
2026-07-28 18:02:47 +0300

    Scripts [unix]: Add 'set -euo pipefail' to exit with error

--- scripts/unix/alembic-proxy.sh
+++ scripts/unix/alembic-proxy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_DIR="$(cd "$SCRIPT_DIR/../.." && pwd)"
 cd "$PROJECT_DIR/Python" || exit 1
